a5e4ac4f8c0aad83b267ba617f74ed8c7acf0c9f
unknown <priyanka.rahate23@gmail.com>
2022-05-11 10:06:32 +0530

    RetrieveByDateRange

--- src/main/java/com/Bridgelabz/ADVAddressBook/AddressBook.java
+++ src/main/java/com/Bridgelabz/ADVAddressBook/AddressBook.java
@@ -1,6 +1,7 @@
 package com.Bridgelabz.ADVAddressBook;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.*;
 
 public class AddressBook {
@@ -65,5 +66,34 @@ public class AddressBook {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+    public List<Contacts> findAllForParticularDate(LocalDate date) {
+        ResultSet resultSet = null;
+        List<Contacts> addressBookList = new ArrayList<Contacts>();
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "select * from AddressBook where date_added between cast(' "+ date + "'" +" as date)  and date(now());";
+            resultSet = statement.executeQuery(sql);
+            int count = 0;
+            while (resultSet.next()) {
+                Contacts contactInfo = new Contacts();
+                contactInfo.setFirstName(resultSet.getString("firstName"));
+                contactInfo.setLastName(resultSet.getString("Lastname"));
+                contactInfo.setAddress(resultSet.getString("address"));
+                contactInfo.setCity(resultSet.getString("city"));
+                contactInfo.setState(resultSet.getString("state"));
+                contactInfo.setZip(resultSet.getInt("zip"));
+                contactInfo.setPhoneNumber(resultSet.getString("phoneNumber"));
+                contactInfo.setEmailId(resultSet.getString("email"));
+                contactInfo.setBookName(resultSet.getString("bookName"));
+                contactInfo.setContactType(resultSet.getString("contactType"));
+                contactInfo.setDateAdded(resultSet.getDate("Date_added").toLocalDate());
+
+                addressBookList.add(contactInfo);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return addressBookList;
     }
 }
